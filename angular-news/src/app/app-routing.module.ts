a0b8aecdf2686d8752e30fce4916f69c1336f261
ahany42 <ahanyfathy42@gmail.com>
2024-08-19 13:22:05 +0200

    Navigation

--- angular-news/src/app/app-routing.module.ts
+++ angular-news/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './header/home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyNewsComponent } from './my-news/my-news.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path:  'myNews',component :MyNewsComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-news/src/app/app.module.ts
+++ angular-news/src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './header/home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyNewsComponent } from './my-news/my-news.component';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    MyNewsComponent,
+    HomeLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
