f784830cad2fbc9eb639284be7f1805f3344c92b
ahany42 <ahanyfathy42@gmail.com>
2024-08-19 13:53:57 +0200

    Files Structure Modified

--- angular-news/src/app/app-routing.module.ts
+++ angular-news/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './header/home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MyNewsComponent } from './my-news/my-news.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { MyNewsComponent } from './Components/my-news/my-news.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
   { path: 'home', component: HomeComponent },
--- angular-news/src/app/app.module.ts
+++ angular-news/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './header/home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MyNewsComponent } from './my-news/my-news.component';
-import { HomeLayoutComponent } from './home-layout/home-layout.component';
-
+import { HeaderComponent } from './Components/header/header.component';
+import { AboutComponent } from './Components/about/about.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { MyNewsComponent } from './Components/my-news/my-news.component';
+import { HomeLayoutComponent } from './Components/home-layout/home-layout.component';
+import { HomeComponent } from './Components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
